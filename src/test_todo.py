fd75917fba200343320db46d0e6327e70cf47e9f
Rayanne Figueiredo <rayannevfs@gmail.com>
2025-11-07 20:38:49 -0300

    teste de validacao de titulo

--- src/test_todo.py
+++ src/test_todo.py
@@ -9,3 +9,9 @@ def test_criar_tarefa_valida():
     assert t["titulo"] == "Estudar TDD"
     assert t["descricao"] == "Ler sobre RED-GREEN-REFACTOR"
     assert t["status"] == STATUS_PENDENTE
+
+def test_nao_permite_titulos_duplicados_case_insensitive():
+    criar_tarefa("Pagar Boleto", "Conta de luz")
+    with pytest.raises(ValueError) as e:
+        criar_tarefa("pagar boleto", "Conta de água")
+    assert "Já existe tarefa com este título." in str(e.value)
